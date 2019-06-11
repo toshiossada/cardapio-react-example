316ed04afa0c241125ec4ddf10e9f8f24336230b
toshiossada <toshiossada@gmail.com>
2019-06-11 09:13:47 -0300

    .

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,3 @@
-import React, { Component } from 'react';
-import { View, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import HomeList from './HomeList';
